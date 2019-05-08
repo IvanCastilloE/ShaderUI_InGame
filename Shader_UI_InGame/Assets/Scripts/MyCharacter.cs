8239d18d882bb82a7f1dbebbf43c571dc8c3efa8
Angel Ivan Castillo <angel_xayan06@hotmail.com>
2019-05-07 19:56:11 -0700

    Creamos un Slider que cambia el shader

---
+++ Shader_UI_InGame/Assets/Scripts/MyCharacter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyCharacter : MonoBehaviour
+{
+    Renderer render;
+    [SerializeField]
+    private float colorPower = 1f;
+    [SerializeField]
+
+    Slider sld_colorPower;
+    private void Start()
+    {
+        render = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        render.material.SetFloat ("_ColorPower",sld_colorPower.value);
+    }
+}
